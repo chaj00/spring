13a91735381e6a388cd4f0849c117cc9a8d193a2
kitri <kitri@DESKTOP-V0PKSER>
2016-05-25 16:26:55 +0900

    160525_16:26

--- springjdbc/src/secondexam/ScottTest.java
+++ springjdbc/src/secondexam/ScottTest.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ScottTest {
 	public static void main(String[] args){
-		ApplicationContext container = new ClassPathXmlApplicationContext("config/data2.xml");
+		ApplicationContext container = new ClassPathXmlApplicationContext("config/scott.xml");
 		
 		ScottDAO dao=(ScottDAO)container.getBean("dao");
 		System.out.println("Á÷¿ø¼ö:"+dao.count());
