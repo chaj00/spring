43f402e00dde562c9f3a69992bd0a9189349a1bf
kitri <kitri@DESKTOP-V0PKSER>
2016-05-26 16:24:56 +0900

    160526_16:24

---
+++ frontController/src/member/MemberSelect.java
@@ -0,0 +1,16 @@
+package member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import test.Action;
+
+public class MemberSelect implements Action {
+	public void run(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		System.out.println("MemberSelect");
+		
+	}
+}
---
+++ frontController/src/test/TestServlet.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("요청받기");
+		ServletContext context = getServletContext();
+		System.out.println(context.getContextPath());
+		System.out.println(request.getRequestURI());
+		System.out.println(request.getRequestURL());
+		System.out.println(request.getRemoteAddr());
+		System.out.println(request.getRemoteHost());
+		
+		String rootPath = request.getContextPath();
+		String subPath = request.getRequestURI();
+		String path = subPath.substring(rootPath.length());
+		System.out.println("가공경로:"+path);
+		
+		MyHandlerMapping mapping = new MyHandlerMapping();
+		Action action = mapping.exe(path);
+		action.run(request, response);
+	}
+
+}
