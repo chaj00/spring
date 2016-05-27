069f4e5527018de88e2a26cb2e1a82d5d88bc0d3
kitri <kitri@DESKTOP-V0PKSER>
2016-05-27 17:24:19 +0900

    160527_17:24

--- annotation/src/main/IndexController.java
+++ annotation/src/main/IndexController.java
@@ -1,18 +1,15 @@
 package main;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-//단순 view를 respose하는 것도 스프링프레임워크를 통해서 response될 수 있도록 구현해야
-//스프링에서 제공하는 모든 서비스를 적용할 수 있다.
-public class IndexController extends AbstractController {
-
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
-		return new ModelAndView("emp/index");
+@Controller
+public class IndexController {
+	@RequestMapping("/index.do")
+	public String main(){
+		System.out.println("어노테이션 기반 작업");
+		return "emp/index";
+		
 	}
 
 }
